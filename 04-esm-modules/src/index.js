4a02a595c3ba7508e565aae7a4453171470d1925
kaiserraf <rafaelmachado2507oliveira@hotmail.com>
2026-03-17 10:19:50 -0300

    aula 04

--- 04-esm-modules/src/index.js
+++ 04-esm-modules/src/index.js
@@ -1,6 +1,7 @@
 import funcaoTeste from "./utils/controllerCaronas.js"; // importa só uma função
 import * as database from "./utils/database.js"; // importa varias funções
 import { destructuring, databaseType } from "./utils/controllerPay.js"; // destructuring 
+import * as api from "./utils/api.js";
 
 destructuring();
 database.connectToDatabase("PostgreSQL");
---
+++ 04-esm-modules/src/utils/api.js
@@ -0,0 +1,10 @@
+// exportação direta (guardar dentro de uma constante)
+
+export const key = {
+    value: "121k3hwi",
+    permission: "admin",
+};
+
+export const getDataFromApi = () => {
+    console.log("dados da api sendo buscados...");
+}
