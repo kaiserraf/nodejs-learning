da2a9f8a9a125070da23b0afe65ad7a24f02ca08
kaiserraf <rafaelmachado2507oliveira@hotmail.com>
2026-03-20 21:07:17 -0300

    aula 07

--- 07-npm-scripts-e-variaveis-ambiente/src/data/data.js
+++ 07-npm-scripts-e-variaveis-ambiente/src/data/data.js
@@ -1,9 +1,10 @@
 async function connectDatabase(user, password) {
-    if(user === "naruto" && password === "uzumaki"){
+    if(user === process.env.USERDATABASE && password === process.env.PASSWORDDATABASE){
         console.log("conectado ao banco de dados com sucesso!");
     }else{
         console.log("falha de login, não foi possivel se conectar ");
     }
 }
-
-export
+export {
+    connectDatabase
+}
--- 07-npm-scripts-e-variaveis-ambiente/src/index.js
+++ 07-npm-scripts-e-variaveis-ambiente/src/index.js
@@ -1,5 +1,9 @@
+import 'dotenv/config';
+import * as database from "./data/data.js";
 async function main() {
-    
+    await database.connectDatabase("japoneslindo", "limdo");
+
+    // console.log(process.env.USERDATABASE);
 }
 
 
